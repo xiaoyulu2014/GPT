ae81ee14de2a65852a70efcaa24ef65e3446859f
Xiaoyu Lu <xiaoyu_lu1992@yahoo.co.uk>
2015-10-14 16:28:11 +0100

    edit

--- R/feature_func.R
+++ R/feature_func.R
@@ -8,24 +8,7 @@ feature = function(x,n,sigmaRBF,generator){
   phi = t(sqrt(2/n)*cos(x * Z + b*2*pi))
   return(phi)
 }
-
-fea_notensor = function(x,m,sigmaRBF,generator){
-  set.seed(generator)
-  D = length(x)
-  w = matrix(rnorm(m*D)/sigmaRBF,m,D)
-  b = runif(m)
-  return(sqrt(2/m)*cos(w %*% x + b*2*pi))
-}
-
-# feature = function(x,n,sigmaRBF,generator){
-#   D = length(x)
-#   set.seed(generator)
-#   Z = matrix(rnorm(n*D)/sigmaRBF,n,D)
-#   b = matrix(runif(n*D),n,D)
-#   phi = sqrt(2/n)*cos(Z * matrix(sample(rep(as.numeric(x),n)),n,D) + b*2*pi)
-#   return(phi)
-# }
-
+#previous bug
 # feature = function(x,n,sigmaRBF,generator){
 #   D = length(x)
 #   set.seed(generator)
